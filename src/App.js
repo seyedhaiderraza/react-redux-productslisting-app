78989c69dbbdcc573628b01561432728610493c3
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-07-12 21:21:52 +0530

    App routes || productlisting route|| single product page route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom'
+import ProductListing from './components/ProductListing';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+   <Router>
+        <Header/>
+      <Routes>
+        <Route path="/" exact element={<ProductListing/>}/>
+        <Route path="/product/:productId" exact element={<ProductDetail/>}/>
+        <Route>404 not found</Route>
+      </Routes>
+   
+    </Router>
+   </div>
   );
 }
 
