cc23205e5bdf2fd8d7c0605bb07a1e58f6ac224b
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-07-12 21:25:50 +0530

    header component and productlisting page, singleproductcomponent page and listing-product card page

---
+++ src/components/ProductComponent.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import {Link} from "react-router-dom"
+const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.products);
+
+  return (
+    <>
+      {products && products.map((product) => (
+        <div  className="four wide column">
+            <Link to={`/product/${product.id}`}>
+              <div className="ui link cards">
+                <div className="card" key={product.id}>
+                        <div className="image">
+                          <img src={product.image} alt={product.title} />
+                        </div>
+                        <div className="content">
+                          <div className="header">{product.title}</div>
+                          <div className="meta price">$ {product.price}</div>
+                          <div className="meta">{product.category}</div>
+                        </div>
+                </div>
+              </div>
+             </Link>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ProductComponent;
---
+++ src/components/ProductListing.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import axios from "axios"
+import {useDispatch, useSelector} from 'react-redux'
+import ProductComponent from './ProductComponent'
+import {addProducts} from '../redux/actions/productActions'
+const ProductListing = () => {
+   // const products = useSelector((state)=>state.allProducts.products)
+    const dispatch = useDispatch()
+
+   const fetchProducts = async () =>{
+
+    const response = await axios
+                            .get("https:///fakestoreapi.com/products")
+                            .catch((err)=>{
+                                console.log("Err", err)
+                            })
+
+    dispatch(addProducts(response.data));
+   }
+
+   useEffect(()=>{
+
+    fetchProducts()
+
+},[])
+  return (
+    <div className="ui grid container">
+        <ProductComponent/>
+    </div>
+  )
+}
+
+export default ProductListing
