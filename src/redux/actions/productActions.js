857aa6ac2e32f980b2839ecb9168fab59c30f6c3
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-07-16 23:58:09 +0530

    c

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -5,10 +5,10 @@ import { actionTypes } from "../constants/actionTypes"
 export const asyncFetchSingleProduct = (productId)=>{
     return async (dispatch)=>{
         const response = await fakeStoreApi.get(`/products/${productId}`)
-       
+   
         dispatch({
             type: actionTypes.FETCH_ASYNC_SINGLE_PRODUCT,
-            payload:response.data
+            payload:await response.data
         })
     }
 }
@@ -18,11 +18,10 @@ export const asyncFetchProducts=()=>{
 
     return async(dispatch)=>{
         const response = await fakeStoreApi.get("/products");
-        
-    
+       
     dispatch( {
         type: actionTypes.FETCH_ASYNC_PRODUCTS,
-        payload: response.data,
+        payload:await response.data,
     })
     }
 
